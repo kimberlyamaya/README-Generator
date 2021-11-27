03f7d69f8c1f606f39aee4279f9893c941881843
kimberlyamaya <kimberly.kimbell89@gmail.com>
2021-11-26 22:57:22 -0800

    created the readME file

--- Develop/index.js
+++ Develop/index.js
@@ -1,23 +1,43 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-//const questions = [];
-// KA - create the array of questions here, do I include object brackets around each question? Do I need to include type and name of each? Or
-// KA - is that only for prompts?
-inquirer.prompt ([
-    {
-    type: "input",
-    name: "title",
-    message: "What is the title of your project?"
-    }
-])
+function questions () {  
+    return inquirer.prompt ([
+        {
+        type: "input",
+        name: "title",
+        message: "What is the title of your project?"
+        },
+
+        {
+        type: "input",
+        name: "description",
+        message: "Enter a description for your project."
+        }
+    ])
+
+    // KA - don't need this here, will use this down below when I call this function questions()
+    .then (function (answers) {
+        init(answers);
+        console.log(answers.title)
+        console.log(answers.description)
+    })
+};
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(answers) {
+    return `# ${answers.title}`
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init(answers) {
+    fs.writeFile('readME.md', writeToFile(answers), err => {
+        if (err) throw err;
+        console.log("you did it")
+    })
+}
 
 // Function call to initialize app
-init();
+questions();
