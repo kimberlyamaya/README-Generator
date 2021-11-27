c626120039afa0ab96271b809f0fa2d08df0a923
kimberlyamaya <kimberly.kimbell89@gmail.com>
2021-11-26 21:26:47 -0800

    intialized inquirer

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,17 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+//const questions = [];
+// KA - create the array of questions here, do I include object brackets around each question? Do I need to include type and name of each? Or
+// KA - is that only for prompts?
+inquirer.prompt ([
+    {
+    type: "input",
+    name: "title",
+    message: "What is the title of your project?"
+    }
+])
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
